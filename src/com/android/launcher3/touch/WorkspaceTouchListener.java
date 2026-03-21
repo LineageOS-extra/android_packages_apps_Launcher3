3e27bc1252cbe87bd990ebbaad5391e857f8fccb
Vivekachooz <vivekachooz@gmail.com>
2026-03-21 08:53:16 +0530

    Revert "Launcher3: Double tap on home screen to turn off screen"
    This reverts commit f854254a6615f720f1ee794ad2b859f500f58793.
    
    Change-Id: Idf8e6dae6f7a76d6bb20d9a878286eab3bcdc171

--- src/com/android/launcher3/touch/WorkspaceTouchListener.java
+++ src/com/android/launcher3/touch/WorkspaceTouchListener.java
@@ -30,7 +30,6 @@ import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCH
 
 import android.graphics.PointF;
 import android.graphics.Rect;
-import android.os.PowerManager;
 import android.view.GestureDetector;
 import android.view.HapticFeedbackConstants;
 import android.view.InputDevice;
@@ -43,7 +42,6 @@ import com.android.launcher3.AbstractFloatingView;
 import com.android.launcher3.CellLayout;
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.Launcher;
-import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.Workspace;
 import com.android.launcher3.dragndrop.DragLayer;
 import com.android.launcher3.logger.LauncherAtom;
@@ -76,8 +74,6 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
 
     private int mLongPressState = STATE_CANCELLED;
 
-    private final PowerManager mPowerManager;
-
     private final GestureDetector mGestureDetector;
 
     public WorkspaceTouchListener(Launcher launcher, Workspace<?> workspace) {
@@ -86,7 +82,6 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
         // Use twice the touch slop as we are looking for long press which is more
         // likely to cause movement.
         mTouchSlop = 2 * ViewConfiguration.get(launcher).getScaledTouchSlop();
-        mPowerManager = workspace.getContext().getSystemService(PowerManager.class);
         mGestureDetector = new GestureDetector(workspace.getContext(), this);
     }
 
@@ -226,14 +221,5 @@ public class WorkspaceTouchListener extends GestureDetector.SimpleOnGestureListe
                 cancelLongPress();
             }
         }
-    }
-
-    @Override
-    public boolean onDoubleTap(MotionEvent event) {
-        if (LauncherPrefs.SLEEP_GESTURE.get(mWorkspace.getContext())) {
-            mPowerManager.goToSleep(event.getEventTime());
-            return true;
-        }
-        return false;
     }
 }
